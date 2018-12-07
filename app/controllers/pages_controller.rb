b7a3806a415be8ed5eac613bff210874fa974879
Fadli Ramadhan <fadlir93@gmail.com>
2018-12-07 19:44:28 +0700

    Restrict Action From UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+    def home
+        redirect_to articles_path if logged_in?
+    end
+
+    def about
+
+    end
+
 end
