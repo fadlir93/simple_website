0b5d247785dd26bc5ab0122b61701019e51ec509
Fadli Ramadhan <fadlir93@gmail.com>
2018-12-07 22:40:48 +0700

    Authentication System [done]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
   def new
     @article = Article.new
@@ -11,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -28,14 +32,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = 'Article was successfully updated'
       redirect_to @article
@@ -45,12 +47,23 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article1 = Article.find(params[:id])
+
   end
 
   private
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end
