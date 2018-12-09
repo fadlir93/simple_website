5959432da5fadc0fc3199cc7289952b9d01f20c1
Fadli Ramadhan <fadlir93@gmail.com>
2018-12-09 10:58:56 +0700

    Create Table Category and Unit test model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_131232) do
+ActiveRecord::Schema.define(version: 2018_12_09_015836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2018_12_08_131232) do
     t.text "about"
     t.string "avatar"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
